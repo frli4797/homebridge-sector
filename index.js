cd3995b87842765ebc970dfd6f8056b3920ef103
Fredrik Jägare Lilja <>
2018-09-28 16:33:10 +0200

    Runnable version. No functionality.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Service, Characteristic;
 
+var pollingtoevent = require("polling-to-event");
+
 module.exports = function(homebridge){
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
@@ -22,16 +24,18 @@ function SectorSecuritySystemAccessory(log, config) {
 	self.securityService = null;
 
 	// debug flag
-	self.debug = config.debug;
+    //self.debug = config.debug;
+    self.debug = true;
 
 	// polling settings
-	self.polling = config.polling;
-	self.pollInterval = config.pollInterval || 30000;
+    self.polling = true;
+	self.pollInterval = config.pollInterval || 3000;
 
 	// cached values
 	self.previousCurrentState = null;
     self.previousTargetState = null;
     
+    self.log("About to initialize.")
     self.init();
 }
 
@@ -40,7 +44,7 @@ function SectorSecuritySystemAccessory(log, config) {
  */
 SectorSecuritySystemAccessory.prototype.init = function() {
 	var self = this;
-
+    self.log("Initilizing...")
 	// set up polling if requested
     if (self.polling) {
         self.log("Starting polling with an interval of %s ms", self.pollInterval);
@@ -90,4 +94,49 @@ SectorSecuritySystemAccessory.prototype.getState = function(callback) {
 	// 		callback(null, parseInt(state));
 	// 	}
 	// }.bind(this));
+};
+
+SectorSecuritySystemAccessory.prototype.getCurrentState = function(callback) {
+    this.log("Getting state")
+    callback(null, 2);
+	// this.httpRequest(url, body, function(error, response, responseBody) {
+	// 	if (error) {
+	// 		this.log("GetState function failed: %s", error.message);
+	// 		callback(error);
+	// 	} else {
+	// 		var state = responseBody;
+	// 		state = this.applyMappers(state);
+	// 		callback(null, parseInt(state));
+	// 	}
+	// }.bind(this));
+};
+
+/**
+ * Identifies the security device (?)
+ *
+ * @param {Function} callback The method to call with the results
+ */
+SectorSecuritySystemAccessory.prototype.identify = function(callback) {
+	this.log("Identify requested!");
+	callback();
+};
+
+/**
+ * Returns the services offered by this security device
+ *
+ * @returns {Array} The services offered
+ */
+SectorSecuritySystemAccessory.prototype.getServices =  function() {
+	this.securityService = new Service.SecuritySystem(this.name);
+
+	this.securityService
+		.getCharacteristic(Characteristic.SecuritySystemCurrentState)
+		.on("get", this.getCurrentState.bind(this));
+
+	// this.securityService
+	// 	.getCharacteristic(Characteristic.SecuritySystemTargetState)
+	// 	.on("get", this.getTargetState.bind(this))
+	// 	.on("set", this.setTargetState.bind(this));
+
+	return [ this.securityService ];
 };
